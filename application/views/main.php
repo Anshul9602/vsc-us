b0be6d8661d4f0930280e4a0e915e190f86faa53
Nidhi <nidhikatkani3@gmail.com>
2024-11-05 18:50:08 +0530

    slider

--- application/views/main.php
+++ application/views/main.php
@@ -1,3 +1,57 @@
+<style>
+    .slider-container {
+        width: 100%;
+        overflow: hidden;
+        position: relative;
+    }
+
+    .slider-wrapper {
+        display: flex;
+        width: calc(14 * 10%);
+        /* 12 items */
+    }
+
+    .slider-item {
+        /* min-width: 17%; */
+        box-sizing: border-box;
+    }
+
+    .slider-item img {
+        /* width: 200px;
+        height: 33px; */
+    }
+
+    .slider {
+        position: absolute;
+        bottom: 10%;
+    }
+
+    /* Overlay styles */
+    .slider-overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 20%;
+        /* Adjust the width to control the overlay size */
+        pointer-events: none;
+        /* Make it non-interactive */
+        background: linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+        z-index: 1;
+        /* Ensure it stays above the slider */
+    }
+
+    .slider-overlay-left {
+        left: 0;
+        background: linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+        /* Fade from white to transparent */
+    }
+
+    .slider-overlay-right {
+        right: 0;
+        background: linear-gradient(to left, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+        /* Fade from white to transparent */
+    }
+</style>
  <!-- Hero -->
  <section id="slider" class="hero p-0 odd">
      <div class="swiper-container no-slider animation slider-h-100 slider-h-auto">
@@ -38,26 +92,58 @@
 
 
  <!-- slider -->
+ <div class="row ">
+                <div class="slider-container">
+                    <!-- Left white overlay -->
+                    <div class="slider-overlay slider-overlay-left"></div>
+
+                    <!-- Slider wrapper -->
+                    <div class="slider-wrapper">
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/QuickBooks.png" class="img-fluid" alt="QuickBooks" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Drake-Software.png" class="img-fluid" alt="Drake Software" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Ultratax.png" class="img-fluid" alt="Ultratax" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Xero.png" class="img-fluid" alt="Xero" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/CCH-Axcess-2.png" class="img-fluid" alt="CCH Axcess" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/ProConnect.png" class="img-fluid" alt="ProConnect" decoding="async" /></div>
+                        <!-- Duplicate images for infinite scroll -->
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Sage.png" class="img-fluid" alt="Sage" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Avalara.png" class="img-fluid" alt="Avalara" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/TaxJar.png" class="img-fluid" alt="TaxJar" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/ADP.png" class="img-fluid" alt="ADP" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Gusto.png" class="img-fluid" alt="Gusto" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Zoho.png" class="img-fluid" alt="Zoho" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Wave.png" class="img-fluid" alt="" decoding="async" /></div>
+                        <div class="slider-item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/AuditFile.png" class="img-fluid" alt="AuditFile" decoding="async" /></div>
+                    
+                    </div>
+
+                    <!-- Right white overlay -->
+                    <div class="slider-overlay slider-overlay-right"></div>
+                </div>
+
+
+            </div>
+
  <div class="client-slider">
-         <div class="container">
-             <div id="client-slider-2" class="owl-carousel owl-theme client-slid client-slider-2">
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/QuickBooks.png" class="img-fluid" alt="QuickBooks" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Drake-Software.png" class="img-fluid" alt="Drake Software" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Ultratax.png" class="img-fluid" alt="Ultratax" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Xero.png" class="img-fluid" alt="Xero" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/CCH-Axcess-2.png" class="img-fluid" alt="CCH Axcess" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/ProConnect.png" class="img-fluid" alt="ProConnect" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Sage.png" class="img-fluid" alt="Sage" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Avalara.png" class="img-fluid" alt="Avalara" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/TaxJar.png" class="img-fluid" alt="TaxJar" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/ADP.png" class="img-fluid" alt="ADP" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Gusto.png" class="img-fluid" alt="Gusto" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Zoho.png" class="img-fluid" alt="Zoho" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/Wave.png" class="img-fluid" alt="" decoding="async" /></div>
-                 <div class="item"><img width="125" height="50" src="https://www.infinity-globus.com/wp-content/uploads/2023/11/AuditFile.png" class="img-fluid" alt="AuditFile" decoding="async" /></div>
-             </div>
+     <div class="container">
+         <div id="client-slider-2" class="owl-carousel owl-theme client-slid client-slider-2">
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
+             <div class="item"></div>
          </div>
      </div>
+ </div>
 
  <!-- Features -->
  <section id="features" class="section-1 features offers featured" style="padding: 80px 0;">
@@ -84,21 +170,21 @@
                      </div>
                  </div>
                  <div class="row items">
-                     <div data-aos="fade-up" class="col-12 col-md-4 p-0 pr-md-4 item">
+                     <div data-aos="fade-up" class="col-12 col-md-4 m-0 p-0 pr-md-4 item">
                          <div data-percent="500" class="radial left">
                              <span></span>
                          </div>
                          <h4>Employees</h4>
                          <!-- <p>Lorem ipsum dolor sit amet.</p> -->
                      </div>
-                     <div data-aos="fade-up" class="col-12 col-md-4 p-0 pr-md-4 item">
+                     <div data-aos="fade-up" class="col-12 col-md-4 m-0 p-0 pr-md-4 item">
                          <div data-percent="200" class="radial left">
                              <span></span>
                          </div>
                          <h4>CPA Clients</h4>
                          <!-- <p>Lorem ipsum dolor sit amet.</p> -->
                      </div>
-                     <div data-aos="fade-up" class="col-12 col-md-4 p-0 pr-md-4 item">
+                     <div data-aos="fade-up" class="col-12 col-md-4 m-0 p-0 pr-md-4 item">
                          <div data-percent="20" class="radial left">
                              <span></span>
                          </div>
@@ -112,6 +198,17 @@
                      <div class="d-sm-inline-flex mb-5 mb-md-0">
                          <!-- <a href="#contact" class="smooth-anchor mx-auto mt-4 btn primary-button">GET IN TOUCH</a> -->
                          <a href="#" class="mx-auto ml-sm-4 mt-4 btn outline-button">EXPLORE MORE</a>
+                     </div>
+                 </div>
+                 <div class="row pt-4">
+                     <div class="col-md-3">
+                         <img src="assets/images/c-1.png" alt="">
+                     </div>
+                     <div class="col-md-3">
+                         <img src="assets/images/c-2.png" alt="">
+                     </div>
+                     <div class="col-md-3">
+                         <img src="assets/images/c-3.png" alt="">
                      </div>
                  </div>
              </div>
@@ -134,36 +231,36 @@
              <div class="col-12 col-md-6 col-lg-4 item">
                  <div class="card">
                      <i class="icon featured icon-people"></i>
-                     <h4 class="m-1">Bookkeeping</h4>
+                     <h4 class="m-1">Accounting</h4>
                      <ul style="list-style: none;">
-                         <li>Bookkeeping Staff</li>
-                         <li>Bookkeeping Senior</li>
-                         <li>Bookkeeping Supervisor</li>
-                         <li>Bookkeeping Manager</li>
+                         <li>Account Staff</li>
+                         <li>Account Senior</li>
+                         <li>Account Supervisor</li>
+                         <li>Account Manager</li>
                      </ul>
                  </div>
              </div>
              <div class="col-12 col-md-6 col-lg-4 item">
                  <div class="card">
                      <i class="icon featured icon-pie-chart"></i>
-                     <h4 class="m-1">SMSF</h4>
+                     <h4 class="m-1">Tax</h4>
                      <ul style="list-style: none;">
-                         <li>SMSF Staff</li>
-                         <li>SMSF Senior</li>
-                         <li>SMSF Supervisor</li>
-                         <li>SMSF Manager</li>
+                         <li>Tax Staff</li>
+                         <li>Tax Senior</li>
+                         <li>Tax Supervisor</li>
+                         <li>Tax Manager</li>
                      </ul>
                  </div>
              </div>
              <div class="col-12 col-md-6 col-lg-4 item">
                  <div class="card">
                      <i class="icon featured icon-speedometer"></i>
-                     <h4 class="m-1">Accounting and Tax</h4>
+                     <h4 class="m-1">Audit</h4>
                      <ul style="list-style: none;">
-                         <li>Accounting and Tax Staff</li>
-                         <li>Accounting and Tax Senior</li>
-                         <li>Accounting and Tax Supervisor</li>
-                         <li>Accounting and Tax Manager</li>
+                         <li>Audit Staff</li>
+                         <li>Audit Senior</li>
+                         <li>Audit Supervisor</li>
+                         <li>Audit Manager</li>
                      </ul>
                  </div>
              </div>
@@ -180,7 +277,8 @@
                  <div class="col-12">
                      <span class="pre-title">We do more for everyone</span>
                      <h2 class="mb-0"> <span class="featured"><span>Why</span></span> Choose Us?</h2>
-                     <p>With an array of effectively efficient services, we aid you in gaining a business foothold and a distinguished mark in the industry. Our unremitting efforts combined with the zeal to offer quality deliverables bring consistent results that boost growth throughout.</p>
+                     <p>Infinity Globus specializes in addressing the needs of CPAs, EAs and accounting firms across the globe.
+                         Our end-to-end outsourced tax and accounting services are designed to help you navigate challenges and seize opportunities.</p>
                  </div>
              </div>
 
@@ -479,3 +577,68 @@
      </div>
  </section>
 
+   <!-- Partners -->
+   <section id="partners" class="section-1 offers testimonials">
+            <div class="container">
+                <div class="row justify-content-center items">
+                    <div data-aos="fade-up" class="col-12 col-md-6 item">
+                        <div class="card">
+                            <div class="col-12">
+                                <img src="assets/images/eBooks.jpg" alt="Logo" class="logo" style="border-radius: 0%;">
+                                <h4>eBooks</h4>
+                                <p>Discover Our Vast Collection of eBooks Explore a World of Knowledge at Your Fingertips</p>
+
+                                <!-- Action -->
+                                <div class="buttons">
+                                    <div class="d-sm-inline-flex">
+                                        <a href="#" class="mt-3 btn outline-button">READ MORE</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-aos="fade-up" class="col-12 col-md-6 item">
+                        <div class="card">
+                            <div class="col-12">
+                                <img src="assets/images/infographics.jpg" alt="Logo" class="logo"  style="border-radius: 0%;">
+                                <h4>Infographics</h4>
+                                <p>Get interesting accounting marketing facts, tips and tools to help you drive new clients to your firm.</p>
+
+                                <!-- Action -->
+                                <div class="buttons">
+                                    <div class="d-sm-inline-flex">
+                                        <a href="#" class="mt-3 btn outline-button">READ MORE</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+
+ 
+<script>
+    const sliderWrapper = document.querySelector('.slider-wrapper');
+let scrollAmount = 1; // Adjust this value to control speed
+
+function startSlider() {
+    // Move the slider to the left continuously
+    sliderWrapper.style.transform = `translateX(${-scrollAmount}px)`;
+    
+    // Keep increasing the scroll amount for smooth movement
+    scrollAmount += 1; // Adjust for speed, higher value = faster scroll
+
+    // If the scroll amount exceeds the width of one set of images, reset it
+    if (scrollAmount >= sliderWrapper.scrollWidth / 2) {
+        scrollAmount = 0; // Reset to start for infinite scrolling effect
+    }
+
+    requestAnimationFrame(startSlider);
+}
+
+// Initialize the slider
+requestAnimationFrame(startSlider);
+
+</script>
