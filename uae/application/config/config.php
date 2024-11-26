863f8214ec77b11d991b465f2d02977c2a82fabb
Nidhi <nidhikatkani3@gmail.com>
2024-11-26 17:21:42 +0530

    update

--- uae/application/config/config.php
+++ uae/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/vsc-us/uae';
+$config['base_url'] = 'http://localhost/vsc-us/';
 // $config['base_url'] = 'https://vscoffshore.com/';
 
 /*
